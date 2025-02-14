cf1f21ca0f10696188fe1e4cbe57194aff17c5d2
James Lagattuta <jimmy.lagattuta@gmail.com>
2025-02-14 00:00:53 -0800

    webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,14 +1,83 @@
+const path = require('path');
 const TerserPlugin = require('terser-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 module.exports = {
   mode: 'production',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: '[name].[contenthash].js',
+    publicPath: '/', // Important for React Router
+    clean: true,
+  },
+  devtool: 'source-map', // Source maps for debugging
+  cache: {
+    type: 'filesystem', // Faster rebuilds
+  },
   optimization: {
     minimize: true,
+    splitChunks: {
+      chunks: 'all',
+    },
     minimizer: [
       new TerserPlugin({
-        terserOptions: require('./.terserrc'), // Load from .terserrc
+        terserOptions: {
+          compress: {
+            drop_console: true,
+            unused: true,
+            dead_code: true,
+          },
+          format: {
+            comments: false,
+          },
+        },
         extractComments: false,
       }),
     ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: 'index.html',
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+      },
+    }),
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'static',
+      openAnalyzer: false,
+      reportFilename: 'bundle-report.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+            cacheDirectory: true, // Speeds up builds
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    alias: {
+      components: path.resolve(__dirname, 'src/components/'),
+      utils: path.resolve(__dirname, 'src/utils/'),
+    },
   },
 };
