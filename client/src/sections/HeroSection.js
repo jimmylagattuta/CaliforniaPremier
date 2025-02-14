34a6664309e7e74d0c619b950f8089e0d8c0ff8d
James Lagattuta <jimmy.lagattuta@gmail.com>
2025-02-13 21:40:08 -0800

    webpack

--- client/src/sections/HeroSection.js
+++ client/src/sections/HeroSection.js
@@ -7,31 +7,31 @@ function HeroSection() {
       <picture>
         {/* Mobile Image (Default) */}
         <source
-          srcSet="https://i.postimg.cc/SxcHYK1g/i-Stock-1069988650-1-2.webp"
+          srcSet="https://i.postimg.cc/Gm8z43zH/i-Stock-1069988650-1-3.webp"
           media="(max-width: 480px)"
           type="image/webp"
         />
 
         {/* Tablet Image */}
         <source
-          srcSet="https://i.postimg.cc/SxcHYK1g/i-Stock-1069988650-1-2.webp"
+          srcSet="https://i.postimg.cc/Gm8z43zH/i-Stock-1069988650-1-3.webp"
           media="(max-width: 768px)"
           type="image/webp"
         />
 
         {/* Desktop Image */}
         <source
-          srcSet="https://i.postimg.cc/SxcHYK1g/i-Stock-1069988650-1-2.webp"
+          srcSet="https://i.postimg.cc/Gm8z43zH/i-Stock-1069988650-1-3.webp"
           media="(min-width: 769px)"
           type="image/webp"
         />
 
         {/* Fallback */}
         <img
-          src="https://i.postimg.cc/SxcHYK1g/i-Stock-1069988650-1-2.webp"
+          src="https://i.postimg.cc/Gm8z43zH/i-Stock-1069988650-1-3.webp"
           alt="Hero"
-          width="100%"
-          height="auto"
+          width="auto"
+          height="100$"
           loading="eager"
         />
       </picture>
