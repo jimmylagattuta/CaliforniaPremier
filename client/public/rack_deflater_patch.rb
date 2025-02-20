bd20f82b1b3b147cf05ffa127792d8802dd786e3
James Lagattuta <jimmy.lagattuta@gmail.com>
2025-02-19 23:36:07 -0800

    inlining critical css in head of index.html

--- client/public/rack_deflater_patch.rb
+++ client/public/rack_deflater_patch.rb
@@ -1,14 +1,31 @@
-# config/initializers/rack_deflater_patch.rb
-
 module DeflaterPatch
     def call(env)
       status, headers, body = super(env)
       if headers["Cache-Control"]
-        # If the header is a Hash, convert it to a string.
         if headers["Cache-Control"].is_a?(Hash)
-          headers["Cache-Control"] = headers["Cache-Control"].map { |k, v| "#{k}=#{v}" }.join(", ") + ", no-transform"
+          log_message = "Cache-Control header is a Hash: #{headers['Cache-Control'].inspect}. Converting to string."
+          if defined?(Rails) && Rails.respond_to?(:logger) && Rails.logger
+            Rails.logger.info log_message
+          else
+            puts log_message
+          end
+          cc_string = headers["Cache-Control"].map { |k, v| "#{k}: #{v}" }.join(", ")
+          headers["Cache-Control"] = cc_string + ", no-transform"
         else
+          log_message = "Cache-Control header is a String: #{headers['Cache-Control']}. Appending directive."
+          if defined?(Rails) && Rails.respond_to?(:logger) && Rails.logger
+            Rails.logger.info log_message
+          else
+            puts log_message
+          end
           headers["Cache-Control"] = headers["Cache-Control"].to_s + ", no-transform"
+        end
+      else
+        log_message = "No Cache-Control header found."
+        if defined?(Rails) && Rails.respond_to?(:logger) && Rails.logger
+          Rails.logger.info log_message
+        else
+          puts log_message
         end
       end
       [status, headers, body]
