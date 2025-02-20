64ada3c68ea288f6f3b3a8e4c125a3d52f0ba97e
James Lagattuta <jimmy.lagattuta@gmail.com>
2025-02-19 19:08:34 -0800

    working below the fold

---
+++ client/src/sections/AboutUsComponent.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./AboutUsComponent.css";
+
+function AboutUsComponent() {
+  return (
+    <section className="about-section">
+      <div className="about-content">
+        <img
+          src="https://i.postimg.cc/qB3ZY64r/i-Stock-1443170756-1-1.webp"
+          alt="About Us"
+          loading="lazy"
+          className="about-image"
+        />
+      </div>
+    </section>
+  );
+}
+
+export default AboutUsComponent;
--- client/src/utilities/Home.js
+++ client/src/utilities/Home.js
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import HeroSection from "../sections/HeroSection";
 import './Home.css';
+const AboutUsComponent = lazy(() => import("../sections/AboutUsComponent"));
 
 function Home() {
   return (
     <div>
       <HeroSection />
+      <Suspense fallback={<div>Loading...</div>}>
+          <AboutUsComponent />
+      </Suspense>
     </div>
   );
 }
