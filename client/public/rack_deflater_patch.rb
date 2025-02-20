eae9ed90877533df3d227cd3f317feef0973aeaa
James Lagattuta <jimmy.lagattuta@gmail.com>
2025-02-19 23:09:06 -0800

    inlining critical css in head of index.html

--- client/public/rack_deflater_patch.rb
+++ client/public/rack_deflater_patch.rb
@@ -1,5 +1,4 @@
 # config/initializers/rack_deflater_patch.rb
-
 module Rack
     class Deflater
       alias_method :original_call, :call
@@ -7,6 +6,11 @@ module Rack
       def call(env)
         status, headers, body = original_call(env)
         if headers["Cache-Control"]
+          # If the Cache-Control header is a Hash, convert it to a string.
+          if headers["Cache-Control"].is_a?(Hash)
+            headers["Cache-Control"] = headers["Cache-Control"].to_s
+          end
+          # Now safely append ", no-transform"
           headers["Cache-Control"] = headers["Cache-Control"].to_s + ", no-transform"
         end
         [status, headers, body]
