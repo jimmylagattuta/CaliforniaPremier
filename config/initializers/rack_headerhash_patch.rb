6a3d7c7bc547b767b12bd91a5ac0b384ed8b662f
James Lagattuta <jimmy.lagattuta@gmail.com>
2025-02-19 22:15:49 -0800

    inlining critical css in head of index.html

--- config/initializers/rack_headerhash_patch.rb
+++ config/initializers/rack_headerhash_patch.rb
@@ -1,5 +1,17 @@
-# config/initializers/rack_headerhash_patch.rb
-unless defined?(Rack::Utils::HeaderHash)
-    Rack::Utils::HeaderHash = Rack::Headers
+# config/initializers/rack_deflater_patch.rb
+
+# Patch Rack::Deflater to safely append ', no-transform' to Cache-Control if it's a string.
+module Rack
+    class Deflater
+      alias_method :original_call, :call
+  
+      def call(env)
+        status, headers, body = original_call(env)
+        if headers["Cache-Control"].is_a?(String)
+          headers["Cache-Control"] = headers["Cache-Control"].to_s + ", no-transform"
+        end
+        [status, headers, body]
+      end
+    end
   end
   
