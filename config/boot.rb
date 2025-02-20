ad88ea3008afb9a73d428432f91f0eabbb2daa6f
James Lagattuta <jimmy.lagattuta@gmail.com>
2025-02-19 23:06:09 -0800

    inlining critical css in head of index.html

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,11 @@
 ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
 
 require "bundler/setup" # Set up gems listed in the Gemfile.
+
+# Patch Rack early to define Rack::Utils::HeaderHash if not already defined
+require "rack"
+unless Rack::Utils.const_defined?(:HeaderHash)
+  Rack::Utils.const_set(:HeaderHash, Rack::Headers)
+end
+
 require "bootsnap/setup" # Speed up boot time by caching expensive operations.
