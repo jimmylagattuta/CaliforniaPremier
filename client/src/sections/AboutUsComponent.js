b7afff346afea5961aea0c39056043e8af11426e
James Lagattuta <jimmy.lagattuta@gmail.com>
2025-02-21 04:13:13 -0800

    our services section added

--- client/src/sections/AboutUsComponent.js
+++ client/src/sections/AboutUsComponent.js
@@ -7,11 +7,11 @@ function AboutUsComponent() {
     <section className="about-section">
       <div className="about-content">
         <img
-          src="https://i.postimg.cc/qB3ZY64r/i-Stock-1443170756-1-1.webp"
+          src="https://i.postimg.cc/NjWGC1nK/i-Stock-1443170756-1-2.webp"
           alt="About Us"
           loading="lazy"
           className="about-image"
-          width="350"
+          width="372"
           height="328"
         />
         <div className="about-us-content-title">
