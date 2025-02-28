b60da3ab02213041da26a155daf05d091eaa5f20
James Lagattuta <jimmy.lagattuta@gmail.com>
2025-02-28 02:30:21 -0800

    Locations page

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  post '/contact', to: 'emails#create'
+  post '/contact', to: 'email#create'
 
   root 'static#index' # React frontend
   get '*path', to: 'static#index', constraints: ->(req) { !req.xhr? && req.format.html? }
