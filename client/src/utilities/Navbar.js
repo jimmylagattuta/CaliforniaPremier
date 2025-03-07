717cd6d6de50c1162be7c3a528c9e760e093a1ae
James Lagattuta <jimmy.lagattuta@gmail.com>
2025-03-07 01:00:37 -0800

    services page done, NAVBAR updated with services

--- client/src/utilities/Navbar.js
+++ client/src/utilities/Navbar.js
@@ -1,33 +1,45 @@
+// src/components/Navbar.jsx
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { servicesData } from '../data';
+import './Navbar.css';
 
 function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
+  const [subMenuOpen, setSubMenuOpen] = useState(false);
   const navigate = useNavigate();
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
+    // Close sub-menu if main menu is closed
+    if (isOpen) {
+      setSubMenuOpen(false);
+    }
   };
 
-  // Handler to navigate and close the menu
+  // Handle navigation and close all menus
   const handleNavItemClick = (path) => {
     navigate(path);
     setIsOpen(false);
+    setSubMenuOpen(false);
+  };
+
+  // Toggle the sub-navbar for "Services"
+  const toggleSubMenu = () => {
+    setSubMenuOpen(!subMenuOpen);
   };
 
   return (
-    <nav className="navbar" style={{ position: "static" }}>
+    <nav className="navbar">
       <div className="navbar-container">
         <div className="navbar-logo" onClick={() => handleNavItemClick('/')}>
           <img
             src="https://i.postimg.cc/mrH0xfnF/i-Stock-1137683414-1-3.webp"
             alt="Company Logo"
-            width="100%" 
-            height="auto" 
-            loading="eager" 
+            loading="eager"
           />
         </div>
-        <div className="menu-icon" alt="Toggle navigation menu" onClick={toggleMenu}>
+        <div className="menu-icon" onClick={toggleMenu}>
           <div className={isOpen ? 'bar change' : 'bar'}></div>
           <div className={isOpen ? 'bar change' : 'bar'}></div>
           <div className={isOpen ? 'bar change' : 'bar'}></div>
@@ -36,8 +48,21 @@ function Navbar() {
           <li className="nav-item" onClick={() => handleNavItemClick('/')}>
             Home
           </li>
-          <li className="nav-item" onClick={() => handleNavItemClick('/services/new-patient-consultation')}>
+          <li className="nav-item" onClick={toggleSubMenu}>
             Services
+            {subMenuOpen && (
+              <ul className="sub-nav-menu">
+                {Object.entries(servicesData).map(([key, service]) => (
+                  <li
+                    key={key}
+                    className="sub-nav-item"
+                    onClick={() => handleNavItemClick(`/services/${key}`)}
+                  >
+                    {service.title}
+                  </li>
+                ))}
+              </ul>
+            )}
           </li>
           <li className="nav-item" onClick={() => handleNavItemClick('/locations')}>
             Locations
