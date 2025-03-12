1303f974ebb890f47fb762bae92b12870413e949
James Lagattuta <jimmy.lagattuta@gmail.com>
2025-03-12 03:24:00 -0700

    pagespeed optimization

--- client/src/index.js
+++ client/src/index.js
@@ -1,27 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import './index.css';
+import React, { startTransition } from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <BrowserRouter basename="/">
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// Register the service worker if available
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
-      .then(registration => {
-        console.log('Service Worker registered with scope:', registration.scope);
-      })
-      .catch(error => {
-        console.error('Service Worker registration failed:', error);
-      });
-  });
-}
+startTransition(() => {
+  root.render(
+    <React.StrictMode>
+      <BrowserRouter basename="/">
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  );
+});
