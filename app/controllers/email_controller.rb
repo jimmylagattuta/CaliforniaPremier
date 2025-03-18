0f294aae40248cb84b66b1c4016e9f882e31fe89
James Lagattuta <jimmy.lagattuta@gmail.com>
2025-03-18 12:40:29 -0700

    california premier url configuring

--- app/controllers/email_controller.rb
+++ app/controllers/email_controller.rb
@@ -1,13 +1,15 @@
 class EmailController < ApplicationController
-
+    skip_before_action :verify_authenticity_token, only: [:create]
+  
     def create
-        contact_params = params.require(:contact).permit(:firstName, :lastName, :email, :phone, :message)
-        puts "Attempting to send email with params: #{contact_params.inspect}"
-        ContactMailer.contact_email(contact_params).deliver_now
-        puts "Email sent successfully"
-        render json: { success: true, message: 'Email sent successfully' }, status: :ok
+      contact_params = params.require(:contact).permit(:firstName, :lastName, :email, :phone, :message)
+      puts "Attempting to send email with params: #{contact_params.inspect}"
+      ContactMailer.contact_email(contact_params).deliver_now
+      puts "Email sent successfully"
+      render json: { success: true, message: 'Email sent successfully' }, status: :ok
     rescue StandardError => e
-        puts "Error sending email: #{e.message}"
-        render json: { success: false, message: 'Error sending email', error: e.message }, status: :unprocessable_entity
+      puts "Error sending email: #{e.message}"
+      render json: { success: false, message: 'Error sending email', error: e.message }, status: :unprocessable_entity
     end
-end
+  end
+  
