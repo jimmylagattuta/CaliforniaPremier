12498d2054e191fd781cea137e671f4c93056515
James Lagattuta <jimmy.lagattuta@gmail.com>
2025-03-18 12:10:39 -0700

    changing domains to live url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -105,7 +105,7 @@ Rails.application.configure do
   # config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.asset_host = "https://californiapremier-5591e4325563.herokuapp.com"
+  config.action_mailer.asset_host = "https://californiapremierpainclinics.com"
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
